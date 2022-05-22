ea396d4f2e3134170b4274c02d2859a8635d6a9d
Élder Müller <elder.muller@gmail.com>
2022-05-21 21:20:35 -0300

    Calculadora simples

---
+++ README.md
@@ -0,0 +1,11 @@
+# Calculadora simples
+___
+
+#### Esse é um projeto de uma calculadora simples, que possui as quatro funções básicas (adição, subtração, multiplicação e divisão).
+
+___
+![](https://i.imgur.com/bEARFCU.jpg)
+___
+
+
+#### Foi feito utilizando apenas HTML, CSS e JavaScript.
---
+++ script.js
@@ -0,0 +1,14 @@
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+function calculate() {
+    const p = document.getElementById("result").value;
+    const q = eval(p);
+
+    document.getElementById("result").value = q;
+}
